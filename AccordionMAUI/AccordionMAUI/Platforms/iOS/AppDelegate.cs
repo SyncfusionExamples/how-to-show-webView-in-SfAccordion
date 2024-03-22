1a72486cf111151a36a1a6f8cb051eb9a8f2abfe
SuthiYuvaraj <92777079+SuthiYuvaraj@users.noreply.github.com>
2024-03-22 09:48:15 +0530

    Added sample for displaying the webView in SfAccordion
    Added sample for displaying the webView in SfAccordion

---
+++ AccordionMAUI/AccordionMAUI/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AccordionMAUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
